ca455003cf987319fe36cda692fc104de2d97469
leonardo <boleo88@gmail.com>
2024-05-18 08:44:15 +0200

    reduce number of patches to calculate in get_binding_propensity

--- get_binding_propensity.py
+++ get_binding_propensity.py
@@ -10,8 +10,10 @@ np.seterr(divide='ignore', invalid='ignore')
 
 class BindingPropensity:
     def __init__(self, surface_file1, surface_file2, output_path):
-        self.surface1 = surface_file1
-        self.surface2 = surface_file2
+        self.surface1 = pd.read_csv(surface_file1)
+        self.surface2 = pd.read_csv(surface_file2)
+        self.file_name1 = Path(surface_file1).stem
+        self.file_name2 = Path(surface_file2).stem
         self.output_path = output_path
 
     @staticmethod
@@ -23,36 +25,37 @@ class BindingPropensity:
         - `surface`: full path of the surface .csv file
         - `verso`: +1 or -1
         """
-        surf = pd.read_csv(surface)
+        surf = surface
         nrow = len(surf.index)
-        coeff_array = np.zeros((nrow, 121))
+        coeff_array = np.zeros((range(0, nrow, 10), 121))
 
-        for i in tqdm.tqdm(range(0, nrow, 200)):
+        # for i in tqdm.tqdm(range(0, nrow, 200)):
+        for i in tqdm.tqdm(range(0, nrow, 10)):
             coeff, _, _, _ = zp.get_zernike(
                 surf[['x', 'y', 'z', 'nx', 'ny', 'nz']], 6.0, i, 20, int(verso)
             )
             coeff_array[i, :] = coeff
 
-        return surf.iloc[range(0, nrow, 200)], coeff_array
+        return surf.iloc[range(0, nrow, 10)], coeff_array
     
     @staticmethod
-    def get_bp_mask(df, point):
+    def get_bp_mask(df, points):
         """
-        Filter patch points indexes from full surface .csv file
+        Filter patch points indexes from reduced surface dataframe
         
         Paramters
         ---------
-        - `df`: full surface .csv file
-        - `point`: patch points
+        - `df`: filtered surface dataframe
+        - `points`: patch points
 
         Return
         ------
-        - `mask`: indexes of the patch point in full surface .csv file
+        - `mask`: indexes of the patch point in filtered surface dataframe
         """
         mask = np.where((
-            df['x'].isin(point[0]) & 
-            df['y'].isin(point[1]) & 
-            df['z'].isin(point[2])), 1, 0)
+            df['x'].isin(points[:, 0]) & 
+            df['y'].isin(points[:, 1]) & 
+            df['z'].isin(points[:, 2])), 1, 0)
         return mask
 
     
@@ -60,45 +63,42 @@ class BindingPropensity:
         """
         Compute binding propensity
         """
-        file_name1 = Path(self.surface1).stem
-        file_name2 = Path(self.surface2).stem
-
-        print(f'compute Zernike invariants for {file_name1}')
+        print(f'compute Zernike invariants for {self.file_name1}')
         df_surf1, mat_coeff1 = self.get_all_invariants(self.surface1, verso=1)
-        print(f'compute Zernike invariants for {file_name2}')
+        print(f'compute Zernike invariants for {self.file_name2}')
         df_surf2, mat_coeff2 = self.get_all_invariants(self.surface2, verso=-1)
 
         patch1, patch2 = zp.contact_points(
             df_surf1[['x', 'y', 'z']].to_numpy(), 
             df_surf2[['x', 'y', 'z']].to_numpy(), 
-            1
+            3
         )
 
         dist = np.sqrt(((mat_coeff1 - mat_coeff2[:, None])**2).sum(2))
-
+        
         color_1 = np.min(dist, axis=0)
         color_2 = np.min(dist, axis=1)
         bp1 = self.get_bp_mask(df_surf1, patch1)
         bp2 = self.get_bp_mask(df_surf2, patch2)
 
         df_bp1 = pd.DataFrame({
-            'x': df_surf1['x'],
-            'y': df_surf1['y'],
-            'z': df_surf1['z'],
+            'x': df_surf1['x'].to_numpy(),
+            'y': df_surf1['y'].to_numpy(),
+            'z': df_surf1['z'].to_numpy(),
             'color': color_1,
             'bp': bp1
         })
 
         df_bp2 = pd.DataFrame({
-            'x': df_surf2['x'],
-            'y': df_surf2['y'],
-            'z': df_surf2['z'],
+            'x': df_surf2['x'].to_numpy(),
+            'y': df_surf2['y'].to_numpy(),
+            'z': df_surf2['z'].to_numpy(),
             'color': color_2,
             'bp': bp2
         })
 
-        df_bp1.to_csv(Path(self.output_path).joinpath(f'{file_name1}_bp.csv'))
-        df_bp2.to_csv(Path(self.output_path).joinpath(f'{file_name2}_bp.csv'))
+        df_bp1.to_csv(Path(self.output_path).joinpath(f'{self.file_name1}_bp.csv'), index=False)
+        df_bp2.to_csv(Path(self.output_path).joinpath(f'{self.file_name2}_bp.csv'), index=False)
 
 
 def main():
