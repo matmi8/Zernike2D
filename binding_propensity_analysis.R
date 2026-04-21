02698ca4605128e305ccd311ca39970e29640935
leonardo <boleo88@gmail.com>
2026-04-21 08:49:33 +0200

    bug fix in binding_propensity_analysis.R

--- binding_propensity_analysis.R
+++ binding_propensity_analysis.R
@@ -9,14 +9,14 @@ df_surf_bp2 <- read.csv(args[2])
 dist_surf1 <- as.matrix(dist(df_surf_bp1[, c("x", "y", "z")]))
 mean_list1 <- lapply(
   seq_len(nrow(df_surf_bp1)),
-  \(i) mean(df_surf_bp1$color[dist_surf1[i, ] <= 6], na.rm = TRUE)
+  \(i) mean(df_surf_bp1$bp[dist_surf1[i, ] <= 6], na.rm = TRUE)
 )
 rm(dist_surf1)
 
 dist_surf2 <- as.matrix(dist(df_surf_bp2[, c("x", "y", "z")]))
 mean_list2 <- lapply(
   seq_len(nrow(df_surf_bp2)),
-  \(i) mean(df_surf_bp2$color[dist_surf2[i, ] <= 6], na.rm = TRUE)
+  \(i) mean(df_surf_bp2$bp[dist_surf2[i, ] <= 6], na.rm = TRUE)
 )
 rm(dist_surf2)
 
@@ -26,8 +26,8 @@ get_auc_rocr <- function(means, bp) {
   return(1 - auc_rocr@y.values[[1]])
 }
 
-auc_rocr1 <- get_auc_rocr(unlist(mean_list1), df_surf_bp1$bp)
-auc_rocr2 <- get_auc_rocr(unlist(mean_list2), df_surf_bp2$bp)
+auc_rocr1 <- get_auc_rocr(unlist(mean_list1), df_surf_bp1$bs)
+auc_rocr2 <- get_auc_rocr(unlist(mean_list2), df_surf_bp2$bs)
 
 print(paste("The AUC of the first protein is:", round(auc_rocr1, 2)))
 print(paste("The AUC of the second protein is:", round(auc_rocr2, 2)))
